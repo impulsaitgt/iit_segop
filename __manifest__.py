a4883e28d80ff51073ca522950448eb110936140
Alexander Paiz <impulsaitgt@gmail.com>
2022-07-06 14:43:08 -0600

    INicio del proyecto de seguridad especial para operaciones Odoo

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'Seguridad especializada de operaciones con contraseña Impulsa IT',
+    'summary':"""
+        Seguridad especializada de operaciones con contraseña Impulsa IT
+    """,
+    'author':'Alexander Paiz',
+    'category': 'General',
+    'version' : '1.0.0',
+    'depends': [
+        "account",
+    ],
+    'data': [
+    ]
+}
